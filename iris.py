bf1dac3aebf3fe651ed7406a6aafa19efef07669
skjha98 <skjha98@gmail.com>
2018-06-16 19:44:53 +0530

    Third Commit Implementing KNeighborsClassifier

--- iris.py
+++ iris.py
@@ -1,7 +1,7 @@
 # Importing Libraries
 from sklearn.datasets import load_iris
 import numpy as np
-from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
@@ -12,7 +12,7 @@ dataset = load_iris()
 data_train, data_test, target_train, target_test = train_test_split(dataset.data, dataset.target, test_size = 0.2)
 
 # Training the classifier
-clf = tree.DecisionTreeClassifier()
+clf = KNeighborsClassifier()
 clf.fit(data_train, target_train)
 
 # Predicting the test results
