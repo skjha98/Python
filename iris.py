d51afd305041d8474d9d8f2540304f80711d16d5
skjha98 <skjha98@gmail.com>
2018-06-17 12:23:55 +0530

    Fourth Commit SexyClassifier Random

--- iris.py
+++ iris.py
@@ -1,7 +1,23 @@
+# Creating own Classifier KNN
+import random
+class SexyClassifier():
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X_test):
+        prediction = []
+        for row in X_test:
+            label = random.choice(self.y_train)
+            prediction.append(label)
+        return prediction
+
+
+
 # Importing Libraries
 from sklearn.datasets import load_iris
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
@@ -12,7 +28,7 @@ dataset = load_iris()
 data_train, data_test, target_train, target_test = train_test_split(dataset.data, dataset.target, test_size = 0.2)
 
 # Training the classifier
-clf = KNeighborsClassifier()
+clf = SexyClassifier()
 clf.fit(data_train, target_train)
 
 # Predicting the test results
