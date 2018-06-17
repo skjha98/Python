4fb014eb7b12dcd199ed81687ff7dc681a4e4172
skjha98 <skjha98@gmail.com>
2018-06-17 22:02:00 +0530

    Fifth Commit SexyClassifier KNeighbour Implementation

--- iris.py
+++ iris.py
@@ -1,5 +1,8 @@
 # Creating own Classifier KNN
 import random
+from scipy.spatial import distance
+def euc(a,b):
+    return distance.euclidean(a,b)
 class SexyClassifier():
     def fit(self, X_train, y_train):
         self.X_train = X_train
@@ -8,9 +11,20 @@ class SexyClassifier():
     def predict(self, X_test):
         prediction = []
         for row in X_test:
-            label = random.choice(self.y_train)
+            label = self.closest(row)
             prediction.append(label)
         return prediction
+
+    def closest(self, row):
+        best_dist = euc(row, self.X_train[0])
+        best_index = 0
+        for i in range (1, len(self.X_train)):
+            dist = euc(row,self.X_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.y_train[best_index]
+
 
 
 
